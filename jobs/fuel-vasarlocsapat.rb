8b68ce459f4fa4997dad10543ee3d2bcbd239697
Andras Kora <ak@akora.info>
2019-05-02 18:05:05 +0200

    Change vasarlocsapat fuel price widget to use Number widget

--- jobs/fuel-vasarlocsapat.rb
+++ jobs/fuel-vasarlocsapat.rb
@@ -1,12 +1,15 @@
 require 'nokogiri'
 require 'httparty'
 
-SCHEDULER.every '4h' do
+fuel_price_only = 0
+
+SCHEDULER.every '2m' do
+  last_price = fuel_price_only
   url = "http://www.vasarlocsapat.hu/_hirek/_uzemanyag/uzemanyag-arak.shtml"
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   fuel_price = parsed_page.css('th')[1].text
-  fuel_price_only = fuel_price.slice(10..12)
+  fuel_price_only = fuel_price.slice(10..12).to_f
   # puts fuel_price_only
-  send_event('fuelprice-vasarlocsapat', { value: fuel_price_only.to_f} )
+  send_event('fuelprice-vasarlocsapat', { current: fuel_price_only.to_f, last: last_price.to_f })
 end
