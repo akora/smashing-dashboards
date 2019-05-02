281df296655bcee10c64e9364a86f5f400151fc3
Andras Kora <ak@akora.info>
2019-05-02 07:14:58 +0200

    Change holtankoljak fuel price widget to use Number widget

--- jobs/fuel-holtankoljak.rb
+++ jobs/fuel-holtankoljak.rb
@@ -1,12 +1,15 @@
 require 'nokogiri'
 require 'httparty'
 
-SCHEDULER.every '4h' do
+fuel_price_only = 0
+
+SCHEDULER.every '2m' do
+  last_price = fuel_price_only
   url = "https://holtankoljak.hu"
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
   fuel_price = parsed_page.css('td.price.avg')[0].css("strong").text
-  fuel_price_only = fuel_price.slice(0..4)
+  fuel_price_only = fuel_price.slice(0..4).to_f
   # puts fuel_price_only
-  send_event('fuelprice-holtankoljak', { value: fuel_price_only.to_f} )
+  send_event('fuelprice-holtankoljak', { current: fuel_price_only.to_f, last: last_price.to_f })
 end
