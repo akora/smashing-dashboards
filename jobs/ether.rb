844ca1ad134782f93927e844cf43ecea58e8bb1a
Andras Kora <ak@akora.info>
2019-05-02 06:41:24 +0200

    Change Ethereum widget to use Number widget

--- jobs/ether.rb
+++ jobs/ether.rb
@@ -2,7 +2,10 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-SCHEDULER.every '30m' do
+eth_price = 0
+
+SCHEDULER.every '2m' do
+  last_price = eth_price
   uri = URI.parse('https://api.coinbase.com/v2/prices/ETH-USD/spot')
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -12,5 +15,5 @@ SCHEDULER.every '30m' do
   eth_price = json_response['data']['amount']
   eth_price = '%.2f' % eth_price.to_f
   #puts eth_price
-  send_event('ethprice', { value: eth_price.to_f} )
+  send_event('ethprice', { current: eth_price.to_f, last: last_price.to_f })
 end
