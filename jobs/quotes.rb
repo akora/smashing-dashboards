d824423ce47b8dcc4791571631a77eed3bef0d44
Andras Kora <ak@akora.info>
2019-05-09 20:15:58 +0200

    Add new widget to display random quotes

---
+++ jobs/quotes.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+SCHEDULER.every '2m' do
+  uri = URI.parse('https://wisdomapi.herokuapp.com/v1/random')
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  json_response = JSON.parse(response.body)
+  author = json_response['author']['name']
+  company = json_response['author']['company']
+  quote = json_response['content']
+  send_event('Quotes', { author: author.to_s, company: company.to_s, quote: quote.to_s })
+end
---
+++ widgets/quotes/quotes.coffee
@@ -0,0 +1,4 @@
+class Dashing.Quotes extends Dashing.Widget
+  @accessor 'author'
+  @accessor 'company'
+  @accessor 'quote'
