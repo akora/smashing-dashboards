ce2014170fca9c8db9176c35c385082f46fdaa34
Andras Kora <ak@akora.info>
2019-05-09 20:38:00 +0200

    Change quote query frequency

--- jobs/quotes.rb
+++ jobs/quotes.rb
@@ -2,7 +2,7 @@ require 'net/http'
 require 'json'
 require 'uri'
 
-SCHEDULER.every '2m' do
+SCHEDULER.every '30m' do
   uri = URI.parse('https://wisdomapi.herokuapp.com/v1/random')
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
